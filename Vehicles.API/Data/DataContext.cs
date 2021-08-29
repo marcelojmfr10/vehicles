8bd8b9971207dd4994b1d249b4ee39ebef1023b8
Jonatan Marcelo Fuentes Rodríguez <marcelofuentes_10@hotmail.com>
2021-08-29 14:26:08 -0600

    Complete DB

--- Vehicles.API/Data/DataContext.cs
+++ Vehicles.API/Data/DataContext.cs
@@ -16,9 +16,12 @@ namespace Vehicles.API.Data
         }
 
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<Detail> Details { get; set; }
         public DbSet<DocumentType> DocumentTypes { get; set; }
+        public DbSet<History> Histories { get; set; }
         public DbSet<Procedure> Procedures { get; set; }
-
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehiclePhoto> VehiclePhotos { get; set; }
         public DbSet<VehicleType> VehicleTypes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -27,6 +30,7 @@ namespace Vehicles.API.Data
             modelBuilder.Entity<Brand>().HasIndex(x => x.Description).IsUnique();
             modelBuilder.Entity<DocumentType>().HasIndex(x => x.Description).IsUnique();
             modelBuilder.Entity<Procedure>().HasIndex(x => x.Description).IsUnique();
+            modelBuilder.Entity<Vehicle>().HasIndex(x => x.Plaque).IsUnique();
             modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
         }
     }
