9106a5c2f980a366cf1d227149e2460498ddc506
Jonatan Marcelo Fuentes Rodríguez <marcelofuentes_10@hotmail.com>
2021-08-29 13:48:50 -0600

    Implement login/logout

--- Vehicles.API/Helpers/IUserHelper.cs
+++ Vehicles.API/Helpers/IUserHelper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Vehicles.API.Data.Entities;
+using Vehicles.API.Models;
 
 namespace Vehicles.API.Helpers
 {
@@ -14,5 +15,7 @@ namespace Vehicles.API.Helpers
         Task CheckRoleAsync(string roleName);
         Task AddUserToRoleAsync(User user, string roleName);
         Task<bool> IsUserInRoleAsync(User user, string roleName);
+        Task<SignInResult> LoginAsync(LoginViewModel model);
+        Task LogoutAsync();
     }
 }
