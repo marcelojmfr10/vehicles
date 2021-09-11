a982829b130e4413b221186cfcd9d2a5dfc5fc62
Jonatan Marcelo Fuentes Rodríguez <marcelofuentes_10@hotmail.com>
2021-09-11 10:21:15 -0600

    Auto register new user

--- Vehicles.API/Controllers/AccountController.cs
+++ Vehicles.API/Controllers/AccountController.cs
@@ -3,18 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
 using Vehicles.API.Helpers;
 using Vehicles.API.Models;
+using Vehicles.Common.Enums;
 
 namespace Vehicles.API.Controllers
 {
     public class AccountController : Controller
     {
         private readonly IUserHelper _userHelper;
+        private readonly DataContext _context;
+        private readonly ICombosHelper _combosHelper;
+        private readonly IBlobHelper _blobHelper;
 
-        public AccountController(IUserHelper userHelper)
+        public AccountController(IUserHelper userHelper, DataContext context, ICombosHelper combosHelper, IBlobHelper blobHelper)
         {
             _userHelper = userHelper;
+            _context = context;
+            _combosHelper = combosHelper;
+            _blobHelper = blobHelper;
         }
 
         public IActionResult Login()
@@ -58,6 +67,73 @@ namespace Vehicles.API.Controllers
         public IActionResult NotAuthorized()
         {
             return View();
+        }
+
+        public IActionResult Register()
+        {
+            AddUserViewModel model = new AddUserViewModel
+            {
+                DocumentTypes = _combosHelper.GetComboDocumentTypes()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(AddUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+
+                User user = await _userHelper.AddUserAsync(model, imageId, UserType.User);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado por otro usuario.");
+                    model.DocumentTypes = _combosHelper.GetComboDocumentTypes();
+                    return View(model);
+                }
+
+                LoginViewModel loginViewModel = new LoginViewModel
+                {
+                    Password = model.Password,
+                    RememberMe = false,
+                    Username = model.Username
+                };
+
+                var resulta2 = await _userHelper.LoginAsync(loginViewModel);
+                if (resulta2.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                //string myToken = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+                //string tokenLink = Url.Action("ConfirmEmail", "Account", new
+                //{
+                //    userid = user.Id,
+                //    token = myToken
+                //}, protocol: HttpContext.Request.Scheme);
+
+                //Response response = _mailHelper.SendMail(model.Username, "Vehicles - Confirmación de cuenta", $"<h1>Vehicles - Confirmación de cuenta</h1>" +
+                //    $"Para habilitar el usuario, " +
+                //    $"por favor hacer clic en el siguiente enlace: </br></br><a href = \"{tokenLink}\">Confirmar Email</a>");
+                //if (response.IsSuccess)
+                //{
+                //    ViewBag.Message = "Las instrucciones para habilitar su cuenta han sido enviadas al correo.";
+                //    return View(model);
+                //}
+
+                //ModelState.AddModelError(string.Empty, response.Message);
+            }
+
+            model.DocumentTypes = _combosHelper.GetComboDocumentTypes();
+            return View(model);
         }
     }
 }
--- Vehicles.API/Helpers/IUserHelper.cs
+++ Vehicles.API/Helpers/IUserHelper.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Vehicles.API.Data.Entities;
 using Vehicles.API.Models;
+using Vehicles.Common.Enums;
 
 namespace Vehicles.API.Helpers
 {
@@ -13,6 +14,7 @@ namespace Vehicles.API.Helpers
         Task<User> GetUserAsync(string email);
         Task<User> GetUserAsync(Guid id);
         Task<IdentityResult> AddUserAsync(User user, string password);
+        Task<User> AddUserAsync(AddUserViewModel model, Guid imageId, UserType userType);
         Task<IdentityResult> UpdateUserAsync(User user);
         Task<IdentityResult> DeleteUserAsync(User user);
         Task CheckRoleAsync(string roleName);
