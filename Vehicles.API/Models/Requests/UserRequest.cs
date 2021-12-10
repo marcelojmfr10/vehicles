446cb40990ed665882970bfa8f93915486cc4b3f
marcelojmfr10 <marcelofuentes_10@hotmail.com>
2021-12-09 19:38:10 -0600

    Add post, put and delete UsersController

---
+++ Vehicles.API/Models/Requests/UserRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Models.Requests
+{
+    public class UserRequest
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Email { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string LastName { get; set; }
+
+        [MaxLength(20, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Document { get; set; }
+
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Address { get; set; }
+
+        [MaxLength(20, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string PhoneNumber { get; set; }
+
+        public byte[] Image { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int DocumentTypeId { get; set; }
+
+        //public string Password { get; set; }
+    }
+}
