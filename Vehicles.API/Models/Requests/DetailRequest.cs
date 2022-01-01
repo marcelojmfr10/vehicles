62220a8af1eaeec0bfec0bf7eb3c39e7bf2e246b
marcelojmfr10 <marcelofuentes_10@hotmail.com>
2022-01-01 15:24:38 -0600

    DetailsController

---
+++ Vehicles.API/Models/Requests/DetailRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Models.Requests
+{
+    public class DetailRequest
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Historia")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int HistoryId { get; set; }
+
+        [Display(Name = "Procedimiento")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int ProcedureId { get; set; }
+
+        [Display(Name = "Precio Mano de Obra")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal LaborPrice { get; set; }
+
+        [Display(Name = "Precio Repuestos")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal SparePartsPrice { get; set; }
+
+        [Display(Name = "Observación")]
+        [DataType(DataType.MultilineText)]
+        public string Remarks { get; set; }
+    }
+}
--- Vehicles.API/Models/Requests/HistoryRequest.cs
+++ Vehicles.API/Models/Requests/HistoryRequest.cs
@@ -13,9 +13,11 @@ namespace Vehicles.API.Models.Requests
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public int VehicleId { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [DisplayFormat(DataFormatString = "{0:N0}")]
         public int Mileage { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Remarks { get; set; }
     }
 }
